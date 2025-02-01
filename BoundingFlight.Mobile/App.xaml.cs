55bccb3145bcb73cd57ef52ccb2450a85c476b10
jparnell <jeremy@jeremyparnell.com>
2025-01-31 21:32:32 -0500

    Project setup.

---
+++ BoundingFlight.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BoundingFlight.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
